fd9f0ec1e844c751254b1876c0f7bdd341804e88
unknown <adenijimujeeb@gmail.com>
2023-02-11 13:01:15 +0000

    tf infra setup

---
+++ iac/environment/dev/providers.tf
@@ -0,0 +1,12 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+  default_tags {
+    tags = {
+      Environment = "dev"
+      ManagedBy   = "terraform"
+      Repo        = "https://github.com/btdigital/tf-setup"
+    }
+  }
---
+++ iac/modules/gh_oidc_module/README.md
@@ -0,0 +1,2 @@
+ 'github_actions_sa@${ PROJECT_ID }.iam.gserviceaccount.com'
+ 'projects/lambda-377114/locations/global/workloadIdentityPools/lambda-pool/providers/gh-oidc-provider'
