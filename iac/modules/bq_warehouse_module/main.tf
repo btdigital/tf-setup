ef7f3d5f51b218fd642fa587bd12157a5a1266bc
unknown <adenijimujeeb@gmail.com>
2023-02-14 22:30:31 +0000

    fixing of the github workflow

--- iac/modules/bq_warehouse_module/main.tf
+++ iac/modules/bq_warehouse_module/main.tf
@@ -4,23 +4,44 @@ resource "google_bigquery_dataset" "bq_raw_staging" {
   friendly_name          = "raw-staging"
   description            = "This dataset is used for storing data to dev."
   default_table_expiration_ms = 2592000000
-  access {
-      role = "OWNER"
-      user_by_email = "serviceAccount:${v}@${var.project_id}.iam.gserviceaccount.com"
-    }
   delete_contents_on_destroy = true
+   labels = {
+    env = "default"
+  }
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
 }
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
+
 resource "google_bigquery_dataset" "bq_dev_dwh" {
   dataset_id             = "bq_dev_dwh"
   location               = var.location
   friendly_name          = "dev-dwh"
   description            = "This dataset is used for storing data to dev."
   default_table_expiration_ms = 2592000000
-  access {
-      role = "OWNER"
-      user_by_email = "serviceAccount:${v}@${var.project_id}.iam.gserviceaccount.com"
-    }
   delete_contents_on_destroy = true
+   labels = {
+    env = "default"
+  }
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
 }
 
 resource "google_bigquery_dataset" "bq_test_dwh" {
@@ -29,9 +50,19 @@ resource "google_bigquery_dataset" "bq_test_dwh" {
   friendly_name          = "test-dwh"
   description            = "This dataset is used for dq checks."
   default_table_expiration_ms = 2592000000
-  access {
-      role = "OWNER"
-      user_by_email = "serviceAccount:${v}@${var.project_id}.iam.gserviceaccount.com"
-    }
   delete_contents_on_destroy = true
+   labels = {
+    env = "default"
+  }
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
 }
