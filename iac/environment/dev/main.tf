14c33a6e33bb181f7a5c541cdaf205721b07901f
unknown <adenijimujeeb@gmail.com>
2023-02-14 23:01:38 +0000

    fixing of the github workflow

--- iac/environment/dev/main.tf
+++ iac/environment/dev/main.tf
@@ -30,12 +30,10 @@ module "cloud-composer" {
   count      = local.cloud_composer_enabled
   project_id = local.project_id
   region     = var.region
-  depends_on = [google_project_service.cloud-composer]
 }
 
 # Data ware house : DWH
 # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
 module "bq_dataset" {
   source = "../../modules/bq_warehouse_module"
-  depends_on = [google_service_account.bq_raw_staging, google_service_account.bq_dev_dwh, google_service_account.bq_test_dwh]
 }
