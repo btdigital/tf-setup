b8066fd95c7186e16d497bab29e629112fae8b5c
unknown <adenijimujeeb@gmail.com>
2023-02-15 00:09:29 +0000

    fixing of the github workflow

--- iac/environment/dev/main.tf
+++ iac/environment/dev/main.tf
@@ -17,20 +17,20 @@ module "oidc" {
 
 }
 
-# Google Compute Engine: VM
-# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
-module "vm_service" {
-  source = "../../modules/google_compute_module"
-}
+# # Google Compute Engine: VM
+# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
+# module "vm_service" {
+#   source = "../../modules/google_compute_module"
+# }
 
-# Cloud Composer Environment
-# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/composer_environment
-module "cloud-composer" {
-  source     = "../../modules/cloud_composer_module"
-  count      = local.cloud_composer_enabled
-  project_id = local.project_id
-  region     = var.region
-}
+# # Cloud Composer Environment
+# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/composer_environment
+# module "cloud-composer" {
+#   source     = "../../modules/cloud_composer_module"
+#   count      = local.cloud_composer_enabled
+#   project_id = local.project_id
+#   region     = var.region
+# }
 
 # Data ware house : DWH
 # Ref: https://registry,.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
--- iac/modules/cloud_composer_module/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "composer_enabled" {
-  description = "composer uri and project id."
-  value       = google_project_service.cloud-composer
-}
-
-output "composer_iamcredentials" {
-  description = "composer authenication."
-  value       = google_project_service.iamcredentials
-}
