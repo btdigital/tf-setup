d232a5a327c98a331b594fccee41849cb4d883e7
unknown <adenijimujeeb@gmail.com>
2023-02-19 00:32:10 +0000

    fixing of the github workflow

--- iac/modules/gh_oidc_module/variables.tf
+++ iac/modules/gh_oidc_module/variables.tf
@@ -20,6 +20,12 @@ variable "project_id" {
   default     = "lambda-377114"
 }
 
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default     = "europe-west6"
+  type        = string
+}
+
 variable "gh_repo" {
   type        = string
   description = "The project repo to create Workload Identity Pool"
